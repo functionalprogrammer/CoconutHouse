849c8e23ba3e490983c8c3d3c43adadaccbad8ca
functionalprog2 <87339722+functionalprogrammer@users.noreply.github.com>
2021-07-12 21:59:36 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+
+func main() {
+  http.Handle("/", http.FileServer(http.Dir("./static")))
+  http.ListenAndServe(":3000", nil)
+
+  fmt.Println("Hello World uwu")
+}
